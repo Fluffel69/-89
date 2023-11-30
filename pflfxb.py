35e8296e1d0f4037ae9170b2aa2f3893471540ff
Fluffel69 <114808361+Fluffel69@users.noreply.github.com>
2023-11-30 23:12:03 +0700

    Update pflfxb.py

--- pflfxb.py
+++ pflfxb.py
@@ -1,12 +1,11 @@
-x=input().split()
-y=input().split()
-ches=0
-znam=0
-if int(x[1])%int(y[1])==0 and int(y[1]) != int(x[1]):
-    znam=int(x[1])
-    ches=int(x[0])+(int(y[0])*int(x[1])%int(y[1])
-elif int(y[1])%int(x[1])==0 and int(y[1]) != int(x[1]):
-        znam=int(y[1])
-        ches=int(y[0])+(int(x[0])*int(y[1])%int(x[1])
-print(ches, znam)
-
+x = input().split()
+y = input().split()
+c = 0
+z = 0
+if int(x[1]) % int(y[1]) == 0 and int(y[1]) != int(x[1]):
+    z = int(x[1])
+    c = int(x[0])+(int(y[0])*(int(x[1]) // int(y[1])))
+elif (int(y[1]) % int(x[1]) == 0) and (int(y[1]) != int(x[1])):
+    z = int(y[1])
+    c = int(y[0])+(int(x[0]) * (int(y[1]) // int(x[1])))
+print(c, z)
